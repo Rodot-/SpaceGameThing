eae594997c3dc706c6aa77fa8a3ebfa8388a00ef
rodot- <jto22@drexel.edu>
2016-06-24 15:07:43 -0400

    Started the math and main modules

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
+#include <string.h>
+#include <sstream>
+
+#define WIDTH 1920
+#define HEIGHT 1080
+#define FPS 24.0
+
+
+int main(int argc, char* argv[]) {
+
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SpaceGameThing");
+	sf::Clock clock;
+
+
+	return 0;
+}
+
