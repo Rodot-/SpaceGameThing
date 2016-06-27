9f2df8e8f6ef75b8bd4d07557d15ad6dc098a5f6
rodot- <jto22@drexel.edu>
2016-06-27 14:35:49 -0400

    Added Scalar field declaration

--- src/Physics/Forces.h
+++ src/Physics/Forces.h
@@ -15,6 +15,7 @@ float* Gravity(physObj* objA, physObj* objB);
 float* Friction(physObj* objA, physObj* objB);
 float* Spring(physObj* objA, physObj* objB);
 
+float* ScalarField(physObj* objA, physObj* objB, std::vector<float> field); //could replace std::vector with some world like object
 
 #endif
 
