c887c9c57dd9b7656a1427210ffc2de948076d90
rodot- <jto22@drexel.edu>
2016-06-27 15:06:16 -0400

    Added getters and setters to the physobjects

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,31 @@ int main(int argc, char* argv[]) {
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SpaceGameThing");
 	sf::Clock clock;
 
+	sf::Texture planetTexture;
+	if (!planetTexture.loadFromFile("../bin/planet.png")) {
+		return -1;
+	}
+	sf::Sprite planet(planetTexture);
+	planet.setOrigin(95,95);
+	planet.setScale(0.5,0.5);
+	planet.setPosition(960,540);
+
+	
+	while (window.isOpen()) {
+	
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		if (clock.getElapsedTime().asSeconds() > 1.0/FPS) {
+			window.clear();
+
+			window.draw(planet);
+			planet.rotate(M_PI/12*0.01);
+			window.display();
+		}
+	}
 
 	return 0;
 }
