044304c4fcaeeff09577b97ea762e6ec70c9127b
rodot- <jto22@drexel.edu>
2016-06-28 15:59:20 -0400

    Tests for animating the walk cycle

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,19 @@
 
 #define WIDTH 1920
 #define HEIGHT 1080
-#define FPS 24.0
+#define FPS 60.0
 
+
+void animate(sf::Sprite* sprite) {
+
+	const sf::Texture* texture = sprite->getTexture();
+	const sf::IntRect intrect = sprite->getTextureRect();
+	sf::Vector2u size = texture->getSize();
+	unsigned int new_left = (intrect.left+128) % size.x;
+	std::cout << new_left << std::endl;
+	sprite->setTextureRect(sf::IntRect(new_left, 0, 128, 128));
+}
+	
 
 int main(int argc, char* argv[]) {
 
@@ -26,6 +37,15 @@ int main(int argc, char* argv[]) {
 	planet.setScale(0.5,0.5);
 	planet.setPosition(960,540);
 
+
+	sf::Texture walkTexture;
+	if (!walkTexture.loadFromFile("../bin/walkSequenceSmall.png")) {
+		return -1;
+	}
+	sf::Sprite man(walkTexture, sf::IntRect(0,0,128,128));
+	man.setOrigin(64,0);
+	man.setPosition(960,540);
+	man.setScale(2,2);
 	
 	while (window.isOpen()) {
 	
@@ -36,9 +56,11 @@ int main(int argc, char* argv[]) {
 		}
 		if (clock.getElapsedTime().asSeconds() > 1.0/FPS) {
 			window.clear();
-
+			clock.restart();
+			window.draw(man);
 			window.draw(planet);
 			planet.rotate(M_PI/4*1.0/FPS);
+			animate(&man);
 			planet.move(std::cos(clock.getElapsedTime().asSeconds())*0.5, 0.5*std::sin(clock.getElapsedTime().asSeconds()));
 			window.display();
 		}
