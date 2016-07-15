666724fbcb79f073f015b6f77cf3fc192e753673
rodot- <jto22@drexel.edu>
2016-07-15 16:55:56 -0400

    removed the old commented-out code.  Also, might want to considering rewriting in a modular way later on

--- bin/animcat.py
+++ bin/animcat.py
@@ -143,28 +143,3 @@ else: final_image = semifinal_image # out final image has not been scaled
 
 final_image.save(outfile, form) #save the image as a png and send it to stdout
 
-
-'''
-if len(sys.argv) == 2:
-
-	f1 = sys.argv[1]
-	Image.fromarray(np.concatenate([np.array(Image.open(f1))[:,i*128:(i+1)*128:,:] for i in xrange(12)], axis = 1)[:,::-1,:]).save("reversed"+f1)
-
-if len(sys.argv) == 5:
-
-	f1 = sys.argv[1] #first file
-	f2 = sys.argv[2] #second file
-	f3 = sys.argv[3] # outfile
-	order = sys.argv[4] #row or column (r or c)
-	axis = 0 if order[0] == 'c' else 1
-
-	Image.fromarray(np.concatenate((np.array(Image.open(f1)), np.array(Image.open(f2))), axis = axis)).save(f3)
-
-elif len(sys.argv) == 3:
-	import glob, re
-	files = glob.glob(sys.argv[1]+'*')
-	Image.fromarray(np.concatenate(map(lambda x: np.array(Image.open(x)), sorted(files, key = lambda x: int(re.findall('\d+',x)[0]))), axis = 1)).save(sys.argv[2])
-	
-print len(sys.argv)
-
-'''
