f73a648e3276c4d1ba8e0f0f5758ba566f72b1b3
rodot- <jto22@drexel.edu>
2016-08-04 15:27:06 -0400

    moved the debug folder and corrected the parameters of the macros from __VA_ARGS__ to (...)

---
+++ src/Debug/Debug.h
@@ -0,0 +1,26 @@
+//small library of debugging functions
+#ifndef DEBUG_H
+#define DEBUG_H
+#include <iostream>
+#include <stdio.h>
+
+#define DEBUG_MSG //are we printing debug messages?
+#define DEBUG_LOG //should we log the debug?
+#define DEBUG_LOG_FILE fopen("dLog.txt","a")
+
+#ifdef DEBUG_LOG
+#define fprintd(stream, ...) fprintf(stream, __VA_ARGS__)
+#else
+#define fprintd(stream, ...)
+#endif
+
+#ifdef DEBUG_MSG
+#define printd(...) printf(__VA_ARGS__); dprintf(DEBUG_LOG_FILE, __VA_ARGS__) //regular print statement
+#define printv(X) std::cout << #X " = " << X << std::endl //print the value of a variable
+#else
+#define printd(...)
+#define printv(X)
+#endif
+
+
+#endif
